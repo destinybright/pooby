cff490bbfc1ec485fea6e018934aa2dc5b469b11
Destiny Bright <130610038+destinybright@users.noreply.github.com>
2024-09-17 19:17:44 -0700

    Update greeneggcrack

--- javascript/crack/greeneggcrack.js
+++ javascript/crack/greeneggcrack.js
@@ -10,11 +10,9 @@ document.getElementById('poke-link').addEventListener('click', function(event) {
     const eggImg = document.getElementById('egg');
 
     if (stage < eggImages.length - 1) {
-        // Increase stage and update egg image
         stage++;
         eggImg.src = eggImages[stage];
     } else {
-        // Redirect to the new page after the last image and another click
         window.location.href = "finalgreencrack.html";
     }
 });
